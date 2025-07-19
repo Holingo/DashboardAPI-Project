04847a1d88cc01ed665939a09b8089e5b143e6ec
Oskar Świątek <pan.holingo@gmail.com>
2025-07-19 17:12:57 +0200

    refactor: added `createUser` function for correct form submission to REST API.

--- ems-frontend/src/services/UserService.js
+++ ems-frontend/src/services/UserService.js
@@ -5,3 +5,7 @@ const REST_API_BASE_URL = 'http://localhost:8080/api/users';
 export const listUsers = () => {
     return axios.get(REST_API_BASE_URL);
 }
+
+export const createUser = (user) => {
+    return axios.post(REST_API_BASE_URL, user);
+}
