b5fee878d8ebf82a3a5ee5e9a59df316c0a8c971
Oskar Świątek <pan.holingo@gmail.com>
2025-07-19 17:15:35 +0200

    refactor(App): changed the interface and added `Router` to make the site work properly.

--- ems-frontend/src/App.tsx
+++ ems-frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListUserComponent from "./components/ListUserComponent.tsx"
 import HeaderComponent from "./components/HeaderComponent"
 import FooterComponent from "./components/FooterComponent"
+import UserComponent from "./components/UserComponent"
 
 function App() {
 
   return (
     <>
-        <HeaderComponent />
-        <ListUserComponent />
-        <FooterComponent />
+        <Router>
+            <HeaderComponent />
+                <Routes>
+                    {/* http://localhost:3000 */}
+                    <Route path='/project' element = { <ListUserComponent /> } />
+                    {/* http://localhost:3000/add-user */}
+                    <Route path='/add-user' element = { <UserComponent /> } />
+                </Routes>
+            <FooterComponent />
+        </Router>
     </>
   )
 }
