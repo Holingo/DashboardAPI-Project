5b71a4474c02c5482973a5e74ce5d20fc0114bd5
Oskar Świątek <pan.holingo@gmail.com>
2025-07-26 19:18:42 +0200

    refactor: fix `SecurityConfig` problem exist.

--- ems-backend/src/main/java/org/vcshine/ems/config/SecurityConfig.java
+++ ems-backend/src/main/java/org/vcshine/ems/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package org.vcshine.ems.config;
 
-import lombok.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.vcshine.ems.service.impl.CustomOAuth2UserService;
 
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Value("${frontend.url}")
     private String frontendUrl;
