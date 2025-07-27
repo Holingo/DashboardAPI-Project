afeb574cb0d099c559b8d7d9cb84e2e1e61e41b6
Oskar Świątek <pan.holingo@gmail.com>
2025-07-27 12:28:58 +0200

    refactor: `header` code update - logo added, panel {title} had to be changed

--- ems-frontend/src/components/HeaderComponent.tsx
+++ ems-frontend/src/components/HeaderComponent.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import logo from '../assets/logo_dashboard.svg';
 
 const HeaderComponent = () => {
     return (
         <div>
-            <header className="mx-auto px-2 bg-black">
-                <a href="/" className="text-2xl font-medium text-white px-10">Users Managment System</a>
-                <nav className="flex sm:justify-center space-x-4 py-4 px-6">
-                    {[
-                        ['Home', '/'],
-                        ['About', '/about'],
-                        ['Project', '/project'],
-                        ['Contact', '/contact'],
-                    ].map(([title, url]) => (
-                        <a href={url} className="rounded-lg px-3 py-2 text-white font-medium hover:bg-slate-100 hover:text-gray-900">{title}</a>
-                    ))}
-                </nav>
+            <header className="bg-[#111416] w-full">
+                <div className="flex items-center justify-between relative px-9 py-8 xl:py-0">
+
+                    <div className="flex items-center space-x-2 z-10">
+                        <a href="/">
+                            <img src={logo} alt="Users Managment System" className="h-20 hidden xl:block" />
+                        </a>
+                    </div>
+
+                    <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex space-x-4">
+                        {[
+                            ['Home', '/'],
+                            ['About', '/about'],
+                            ['Project', '/project'],
+                            ['Contact', '/contact'],
+                        ].map(([title, url]) => (
+                            <a href={url} className="rounded-lg px-3 py-2 text-white font-semibold text-base hover:bg-slate-100 hover:text-gray-900">{title}</a>
+                        ))}
+                    </nav>
+                </div>
             </header>
         </div>
     );
