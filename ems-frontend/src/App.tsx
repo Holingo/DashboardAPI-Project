34899eaefb35716d61ccbec6633b801fb84e55dc
Oskar Świątek <pan.holingo@gmail.com>
2025-07-28 11:55:13 +0200

    refactor: fixed bugs with `Footer` rendering on the page - [serious bug] - improvement required in the future

--- ems-frontend/src/App.tsx
+++ ems-frontend/src/App.tsx
@@ -6,30 +6,30 @@ import FooterComponent from "./components/FooterComponent";
 import UserComponent from "./components/UserComponent";
 import LoginPage from "./components/LoginPage.tsx";
 import PortalComponent from "./components/PortalComponent.tsx";
+import {useEffect} from "react";
+
+import ScrollFadeEffect from "./effects/ScrollFadeEffect.tsx";
+import SmoothScrollHandler from "./effects/SmoothScrollHandler.tsx";
 
 function App() {
-
-  return (
-    <>
-        <Router>
-            <HeaderComponent />
-                <Routes>
-                    {/* http://localhost:3000 */}
-                    <Route path="/" element={ <PortalComponent/> } />
-
-                    {/* http://localhost:3000/login */}
-                    <Route path="/login" element={ <LoginPage /> } />
-
-                    {/* http://localhost:3000/project */}
-                    <Route path='/project' element = { <ListUserComponent /> } />
-
-                    {/* http://localhost:3000/add-user */}
-                    <Route path='/add-user' element = { <UserComponent /> } />
-                </Routes>
-            <FooterComponent />
-        </Router>
-    </>
-  )
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Router>
+                <ScrollFadeEffect />
+                <SmoothScrollHandler />
+                <HeaderComponent />
+                <div className="flex-1 overflow-auto">
+                    <Routes>
+                        <Route path="/" element={<PortalComponent />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/project" element={<ListUserComponent />} />
+                        <Route path="/add-user" element={<UserComponent />} />
+                    </Routes>
+                </div>
+                <FooterComponent />
+            </Router>
+        </div>
+    );
 }
 
 export default App
